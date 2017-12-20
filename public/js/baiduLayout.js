cd693fb3f8555ead00a9e802c31a5cbad1149005
季尔洁 <jierjie@99bicycle.com>
2017-12-20 11:29:11 +0800

    '添加检测'

---
+++ public/js/baiduLayout.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function () {
+  var hm = document.createElement('script')
+  hm.src = 'https://hm.baidu.com/hm.js?bf02fd902396e9a3a33a9d35ad33ae97'
+  var s = document.getElementsByTagName('script')[0]
+  s.parentNode.insertBefore(hm, s)
+})()
